6d8dedb924ee26d3c304f5d54bfc55e80f1271ed
kevduc <kduconge@gmail.com>
2021-09-15 13:46:34 +0100

    works

---
+++ README.md
@@ -0,0 +1,1 @@
+Image from https://i.imgur.com/CK1KizH.jpg
---
+++ cipher-decode.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+const cipher = new Map(require('./cipher.json'))
+const cipheredText = fs.readFileSync('./ciphered.txt', 'utf-8')
+
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
+}
+
+function tokenize(txt, tokens) {
+  return txt.match(new RegExp(`${tokens.map(escapeRegExp).join('|')}|\n|.`, 'g'))
+}
+
+function decipher(txt, cipher) {
+  const tokens = [...cipher.keys()]
+  const deciphered = tokenize(txt, tokens).map((token) => cipher.get(token) ?? token)
+  return deciphered.join('')
+}
+
+const decipheredText = decipher(cipheredText, cipher)
+fs.writeFileSync('./deciphered.txt', decipheredText)
